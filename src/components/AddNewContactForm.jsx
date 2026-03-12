f01b1df5dad691bfa80ae93600be3fd604f264fd
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-12 13:15:20 +0000

    refactor

--- src/components/AddNewContactForm.jsx
+++ src/components/AddNewContactForm.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 function AddNewContactForm() {
 
-    const { store, dispatch } = useGlobalReducer();
+    const { dispatch } = useGlobalReducer();
     const navigate = useNavigate();
     const [addContactInfo, setAddContactInfo] = useState({
         name: '',
@@ -15,6 +15,10 @@ function AddNewContactForm() {
     })
 
     console.log(addContactInfo)
+
+    function handleChange (e) {
+        setAddContactInfo({...addContactInfo, ...{[e.target.id]: e.target.value}})
+    }
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -36,19 +40,19 @@ function AddNewContactForm() {
             <form className="mt-5" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Full Name</label>
-                    <input onChange={(e) => setAddContactInfo({ ...addContactInfo, name: e.target.value })} value={addContactInfo.name} type="text" className="form-control" id="name" aria-describedby="name" required placeholder="Enter Full Name" />
+                    <input onChange={handleChange} value={addContactInfo.name} type="text" className="form-control" id="name" aria-describedby="name" required placeholder="Enter Full Name" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input onChange={(e) => setAddContactInfo({ ...addContactInfo, email: e.target.value })} value={addContactInfo.email} type="email" className="form-control" id="email" required placeholder="Enter Email" />
+                    <input onChange={handleChange} value={addContactInfo.email} type="email" className="form-control" id="email" required placeholder="Enter Email" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="phone" className="form-label">Phone</label>
-                    <input onChange={(e) => setAddContactInfo({ ...addContactInfo, phone: e.target.value })} value={addContactInfo.phone} type="text" className="form-control" id="phone" required placeholder="Enter Phone" />
+                    <input onChange={handleChange} value={addContactInfo.phone} type="text" className="form-control" id="phone" required placeholder="Enter Phone" />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="address" className="form-label">Address</label>
-                    <input onChange={(e) => setAddContactInfo({ ...addContactInfo, address: e.target.value })} value={addContactInfo.address} type="text" className="form-control" id="address" required placeholder="Enter Address" />
+                    <input onChange={handleChange} value={addContactInfo.address} type="text" className="form-control" id="address" required placeholder="Enter Address" />
                 </div>
                 <button type="submit" className="btn btn-primary w-100">Save</button>
 
