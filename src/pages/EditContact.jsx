62772737e5ec56038d554d343f331249759123f7
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-16 13:36:51 +0000

    update

--- src/pages/EditContact.jsx
+++ src/pages/EditContact.jsx
@@ -7,7 +7,7 @@ function EditContact() {
     return (
         <>
             <div className="m-2">
-                <h1 className="text-center my-5 fw-bold">Edit contact page</h1>
+                <h1 className="text-center my-5 fw-bold">Edit contact</h1>
                 <EditContactForm />
                 <Link to={'/'}>
                     <p className="text-center">or get back to contacts</p>
